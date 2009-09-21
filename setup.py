8fcabc5128cf7754dda227e8eeb13beb76a8d770
Matthew Brett <matthew.brett@gmail.com>
2009-09-21 17:11:02 -0400

    URL and description updates

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup
 
 setup(name='transforms3d',
       version='0.1a',
-      description='Semantic vector analysis package',
+      description='3D transforms - possible merge',
       author='Christoph Gohlke, Matthew Brett',
       author_email='Christoph Gohlke, matthew.brett@gmail.com',
-      url='http://imaging.mrc-cbu.cam.ac.uk/svn/transforms3d',
+      url='http://github.com/matthew-brett/transforms3d',
       packages=['transforms3d'],
       package_data={'transforms3d': ['derivations/*.py']},
       scripts=glob('scripts/*.py')
