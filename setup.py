5f7bb6201882201509aac3999c6e50b6e1fa4e6c
Matthew Brett <matthew.brett@gmail.com>
2015-01-07 17:45:41 +0000

    RF: use versioneer for versions

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 ''' Installation script for transforms3d package '''
 import sys
+from os.path import join as pjoin
 
 # For some commands, use setuptools.
 if len(set(('develop', 'bdist_egg', 'bdist_rpm', 'bdist', 'bdist_dumb',
@@ -10,8 +11,17 @@ if len(set(('develop', 'bdist_egg', 'bdist_rpm', 'bdist', 'bdist_dumb',
 
 from distutils.core import setup
 
+import versioneer
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = pjoin('transforms3d', '_version.py')
+versioneer.versionfile_build = pjoin('transforms3d', '_version.py')
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = 'transforms3d-'
+
 setup(name='transforms3d',
-      version='0.1a',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='3D transforms - possible merge',
       author='Christoph Gohlke, Matthew Brett',
       author_email='Christoph Gohlke, matthew.brett@gmail.com',
--- transforms3d/__init__.py
+++ transforms3d/__init__.py
@@ -11,3 +11,7 @@ Additional code monkey work by Matthew Brett
 from . import taitbryan
 from . import affines
 from . import quaternions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
