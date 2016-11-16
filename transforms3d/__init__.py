9e1143ce51b333b87516cc69b589e483de6c6842
Matthew Brett <matthew.brett@gmail.com>
2016-11-16 11:35:39 -0800

    RF: add top-level module imports for all modules
    Fixes gh-13.

--- transforms3d/__init__.py
+++ transforms3d/__init__.py
@@ -8,6 +8,11 @@ Additional code monkey work by Matthew Brett
 from . import taitbryan
 from . import affines
 from . import quaternions
+from . import euler
+from . import axangles
+from . import reflections
+from . import shears
+from . import zooms
 
 from ._version import get_versions
 __version__ = get_versions()['version']
