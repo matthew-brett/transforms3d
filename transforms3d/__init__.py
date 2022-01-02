230a09fa43d4a3c3183e0a514dd3c791701df417
zzz4zzz <karol.szepietowski@gmail.com>
2022-01-02 15:23:54 +0100

    Upgrade versioneer

--- transforms3d/__init__.py
+++ transforms3d/__init__.py
@@ -14,6 +14,5 @@ from . import reflections
 from . import shears
 from . import zooms
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
