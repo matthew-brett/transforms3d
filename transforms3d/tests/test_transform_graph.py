66aa943d4f1994a88f84aae336194caec0b7fb3f
Marat Kopytjuk <kopytjuk@gmail.com>
2022-10-29 16:41:34 +0200

    implement first version of tf graph

--- transforms3d/tests/test_transform_graph.py
+++ transforms3d/tests/test_transform_graph.py
@@ -1,11 +1,12 @@
-from transforms3d.transform_graph import Transformation, create_tranformation_graph
+from transforms3d.transform_graph import Transformation, create_tranformation_graph, get_tranformation_matrix
 import numpy as np
 import igraph as ig
 
 
 def test_create_tranformation_graph():
 
-    T = np.zeros((4,4))
+    # T = np.zeros((4,4), dtype=float)
+    T = np.identity(4)
     wizard_transforms = [
         Transformation("world", "left_hand", T),
         Transformation("world", "right_hand", T),
@@ -16,3 +17,7 @@ def test_create_tranformation_graph():
 
     g = create_tranformation_graph(wizard_transforms)
     assert isinstance(g, ig.Graph)
+
+    H = get_tranformation_matrix(g, "world", "page_corner")
+
+    assert isinstance(H, np.ndarray)
--- transforms3d/transform_graph.py
+++ transforms3d/transform_graph.py
@@ -12,20 +12,61 @@ class Transformation:
     A: np.ndarray  # 4x4 matrix for 3D affine trafo
 
 
+def _invert_tf(tf: Transformation) -> Transformation:
+    R = tf.A[:3, :3]
+    p = tf.A[:3, 3]
+
+    R_inv = np.linalg.inv(R)
+
+    A_inv = np.identity(4, dtype=float)
+    A_inv[:3, :3] = R_inv
+    A_inv[:3, 3] = - R_inv @ p
+
+    return Transformation(tf.target_frame, tf.source_frame, A_inv)
+
+
+def invert_transformations(transformations: List[Transformation]) -> List[Transformation]:
+
+    inverted_transformations = list()
+    for tf in transformations:
+        tf_inv = _invert_tf(tf)
+        inverted_transformations.append(tf_inv)
+    return inverted_transformations
+
+
 def create_tranformation_graph(transformations: List[Transformation]):
 
     unique_frames = set([tf.source_frame for tf in transformations] + [tf.target_frame for tf in transformations])
+    unique_frames = list(unique_frames)
 
-    unique_frames_map = {i: f for f, i in enumerate(list(unique_frames))}
-    unique_frames_map_reverse = {v: k for k, v in unique_frames_map.items()}
+    unique_frames_map = {i: frame for i, frame in enumerate(unique_frames)}
+    unique_frames_map_reverse = {frame: i for i, frame in unique_frames_map.items()}
 
-    tf_pairs_as_ids = set([(unique_frames_map[tf.source_frame], unique_frames_map[tf.target_frame]) \
-        for tf in transformations])
+    transformations_inv = invert_transformations(transformations)
+    transformations_all = transformations_inv + transformations
 
-    tf_graph = ig.Graph(len(unique_frames), list(tf_pairs_as_ids))
-    tf_graph.vs["name"] = [unique_frames_map_reverse[i] for i in range(len(unique_frames))]
+    tf_pairs_as_ids = [(unique_frames_map_reverse[tf.source_frame], unique_frames_map_reverse[tf.target_frame]) \
+        for tf in transformations_all]
+
+    # TODO check for duplicates
+
+    tf_graph = ig.Graph(len(unique_frames), tf_pairs_as_ids, directed=True)
+    tf_graph.vs["name"] = unique_frames
+
+    tf_graph.es["A"] = [tf.A for tf in transformations_all]
 
     return tf_graph
+
+
+def get_tranformation_matrix(tf_graph: ig.Graph, source: str, target: str) -> List[int]:
+
+    vertices_on_path = tf_graph.get_shortest_paths(source, to=target, mode="out", output='vpath')[0]
+    
+    H = np.ones((4, 4), dtype=float)
+    for e0, e1 in pairwise(vertices_on_path):
+        edge = tf_graph.es.find(_source=e0, _target=e1)
+        H = H @ edge["A"]
+    return H
 
 
 def pairwise(iterable):
