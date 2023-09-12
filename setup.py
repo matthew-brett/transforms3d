8b6ffe01878a1389a5212f4ec4014ce9243a6ab4
Chris Markiewicz <effigies@gmail.com>
2023-09-12 11:16:45 +0200

    MNT: Configure package finding/data

--- setup.py
+++ setup.py
@@ -11,10 +11,5 @@ import versioneer
 setup(name='transforms3d',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
-      packages=['transforms3d',
-                'transforms3d.derivations',
-                'transforms3d.tests'],
-      package_data = {'transforms3d':
-                      [pjoin('tests', 'data', '*')]},
       zip_safe=False,
       )
